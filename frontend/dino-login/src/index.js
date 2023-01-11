4ae2cc40102384a8fa9d20ee3ddd67441217bb76
louisglan <louissam@hotmail.co.uk>
2023-01-11 16:02:55 +0000

    removed assets

---
+++ frontend/dino-login/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Components/App.js";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+reportWebVitals();
